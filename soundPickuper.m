59bbdbfb799dbfccd16b720c2d228d8b01ee6466
s_toba <en423542@s.okayama-u.ac.jp>
2014-11-28 18:05:53 +0900

    no message

--- soundPickuper.m
+++ soundPickuper.m
@@ -1,93 +1,128 @@
 clear all;
 %% 前処理
-fname_withoutWAV = '141121_001';
+fname_withoutWAV = '141111_001';
 filename = [fname_withoutWAV,'.WAV'];
 a_info = audioinfo(filename);
 fs = a_info.SampleRate;
 i_stop = floor(a_info.Duration/60)-1;
 
 %% フレームごとのパラメータ取得
+time = [];      % 時間
+sp = [];        % スペクトル
+db = [];        % dB (P0=20*10^-6)
+cent = [];      % スペクトル重心
+chro = [];      % クロマグラム
+deltaT = 0.5;   % フレーム分解能
+shiftT = 0.25;   % フレームシフト長
+fft_size = 4096;% FFTサイズ
+P0 = 2e-5;      % dB計算におけるP0
+
+% パラメータ取得全体での計算時間の計測開始
 t_total = cputime;
-time = [];
-sp = [];
-rms = [];
-cent = [];
-chro = [];
-deltaT = 0.2;
-shiftT = 0.1;
 for i=0:i_stop
+    % 1分ごとのパラメータ取得の計算時間の計測開始
     t_part = cputime;
     display(['calculating ',num2str(i),' to ',num2str(i+1)]);
-    [t_time,t_sp,t_rms,t_cent,t_chro] = soundPickuper_getparameter...
-        (fname_withoutWAV,i*60,(i+1)*60+shiftT);
+    
+    % パラメータ取得
+    [t_time,t_sp,t_db,t_cent,t_chro] = soundPickuper_getparameter...
+        (fname_withoutWAV,i*60,(i+1)*60+shiftT,deltaT,shiftT,fft_size);
     sp = cat(1,sp,t_sp);
     time = cat(1,time,t_time);
-    rms = cat(1,rms,t_rms);
+    db = cat(1,db,t_db);
     cent = cat(1,cent,t_cent);
     chro = cat(1,chro,t_chro);
+    
+    % 1分ごとのパラメータ取得の計算時間の計測終了
     t_part = cputime - t_part;
     display(['計算時間は ',num2str(t_part),' 秒です']);
 end
+% パラメータ取得全体での計算時間の計測終了
 t_total = cputime - t_total;
 display(['トータルの計算時間は ',num2str(t_total),' 秒です']);
 
 %% 点数計算
-sec = 30/shiftT;   % スコア2の中央値と標準偏差をとるデータの間隔 in sample
-med_rms = median(rms);
-sd_rms = std(rms);
-med_cent = median(cent);
-sd_cent = std(cent);
+% スコア2の中央値と標準偏差をとるデータの間隔 in sample
+sec = 30/shiftT;    
+
+% 一定以上のdBを持つインデックスのみを取り出す
+thsld = 0.1;                       % threshold in amplitude
+thsld_db = 10*log10(thsld^2/P0^2);  % in db
+upperthsld = (db>=thsld_db);
+db_upperthsld = db(upperthsld);
+cent_upperthsld = cent(upperthsld);
+
+% スコア1の計算に用いる，全体を見たときの中央値と標準偏差
+med_db = median(db_upperthsld);
+sd_db = std(db_upperthsld);
+med_cent = median(cent_upperthsld);
+sd_cent = std(cent_upperthsld);
+
 for i=1:length(time)
     % スコア1の計算
-    score1_rms(i) = detcurve(rms(i),med_rms,sd_rms);
+    score1_db(i) = detcurve(db(i),med_db,sd_db);
     score1_cent(i) = detcurve(cent(i),med_cent,sd_cent);
-    score1(i) = getscore(score1_rms(i),score1_cent(i));
+    score1(i) = getscore(score1_db(i),score1_cent(i));
     
-    % スコア2の計算
+    % 現在のインデックスを中心として，一定区間をとる
     len = length(time);
     if i>sec && i<=(len-sec)
-        tmp_rms = rms((i-sec):(i+sec));
+        tmp_db = db((i-sec):(i+sec));
         tmp_cent = cent((i-sec):(i+sec));
     elseif i<=sec && i<=(len-sec)
-        tmp_rms = [rms(1:i);rms((i+1):(i+sec))];
+        tmp_db = [db(1:i);db((i+1):(i+sec))];
         tmp_cent = [cent(1:i);cent((i+1):(i+sec))];
     elseif i>sec && i>(len-sec)
-        tmp_rms = [rms((i-sec):(i-1));rms(i:len)];
+        tmp_db = [db((i-sec):(i-1));db(i:len)];
         tmp_cent = [cent((i-sec):(i-1));cent(i:len)];
     end
-    med_tmp_rms = median(tmp_rms);
-    sd_tmp_rms = std(tmp_rms);
-    med_tmp_cent = median(tmp_cent);
-    sd_tmp_cent = std(tmp_cent);
-    score2_rms(i) = detcurve(rms(i),med_tmp_rms,sd_tmp_rms);
+    
+    tmp_upperthsld = (tmp_db>=thsld_db);
+    tmp_db = tmp_db(tmp_upperthsld);
+    tmp_cent = tmp_cent(tmp_upperthsld);
+    
+    % 一定区間の中央値，標準偏差をとる
+    if isempty(tmp_db)
+        med_tmp_db = 0;
+        sd_tmp_db = 0;
+        med_tmp_cent = 0;
+        sd_tmp_cent = 0;
+    else
+        med_tmp_db = median(tmp_db);
+        sd_tmp_db = std(tmp_db);
+        med_tmp_cent = median(tmp_cent);
+        sd_tmp_cent = std(tmp_cent);
+    end
+    
+    % スコア2の計算
+    score2_db(i) = detcurve(db(i),med_tmp_db,sd_tmp_db);
     score2_cent(i)= detcurve(cent(i),med_tmp_cent,sd_tmp_cent);
-    score2(i) = getscore(score2_rms(i),score2_cent(i));
+    score2(i) = getscore(score2_db(i),score2_cent(i));
 end
 
 %%  テーブル作成
-T = table(time,rms,cent,score1',score2','VariableNames',...
-    {'time','rms','cent','score1','score2'});
-% T = sortrows(T,'total','descend');
+score_total = score1+score2;
+T = table(time,db,cent,score1',score2',score_total','VariableNames',...
+    {'time','dB','cent','score1','score2','total'});
 
-%% プロット
-% samplePerMin = 60/shiftT;
-% coeff = ones(1,samplePerMin)/samplePerMin;
-% sc1 = score1;
-% sc2 = sc1+score2;
-% score12 = (score1/100-score2/100).^2;
-% sc1_filtered = filter(coeff,1,sc1);
-% sc2_filtered = filter(coeff,1,sc2);
-% score12_filtered = filter(coeff,1,score12);
-% 
-% t = linspace(time(1),time(end)/60,length(time));
-% figure;
-% subplot(2,1,1);
-% plot(t,sc1_filtered,'DisplayName','sc1_filtered');hold all;...
-%     plot(t,sc2_filtered,'DisplayName','sc2_filtered');hold off;
-% xlim([0,time(end)/60]);
-% subplot(2,1,2);
-% plot(t,score12_filtered,'DisplayName','score12_filtered');
-% xlim([0,time(end)/60]);
+% クロマトーンの取得
+T_chro = table(time,chro(:,1),chro(:,2),chro(:,3),chro(:,4),chro(:,5),...
+    chro(:,6),chro(:,7),chro(:,8),chro(:,9),chro(:,10),chro(:,11),chro(:,12),...
+    'VariableNames',...
+    {'time','C','Csharp','D','Dsharp','E','F','Fsharp','G','Gsharp','A','Asharp','B'});
+thsld_chro = 0.999;
+chromatone.C        = T_chro.time(T_chro.C>=thsld_chro);
+chromatone.Csharp   = T_chro.time(T_chro.Csharp>=thsld_chro);
+chromatone.D        = T_chro.time(T_chro.D>=thsld_chro);
+chromatone.Dsharp   = T_chro.time(T_chro.Dsharp>=thsld_chro);
+chromatone.E        = T_chro.time(T_chro.E>=thsld_chro);
+chromatone.F        = T_chro.time(T_chro.F>=thsld_chro);
+chromatone.Fsharp   = T_chro.time(T_chro.Fsharp>=thsld_chro);
+chromatone.G        = T_chro.time(T_chro.G>=thsld_chro);
+chromatone.Gsharp   = T_chro.time(T_chro.Gsharp>=thsld_chro);
+chromatone.A        = T_chro.time(T_chro.A>=thsld_chro);
+chromatone.Asharp   = T_chro.time(T_chro.Asharp>=thsld_chro);
+chromatone.B        = T_chro.time(T_chro.B>=thsld_chro);
 
 %% ランダムピックアップ
 sample_pickup = int32(10/shiftT)-1;
@@ -98,29 +133,52 @@ r = range_time-sample_pickup;
 rng('shuffle');
 for i=1:num_pickup
     R(i) = randi([1,r],1);
-    R_score1(i) = mean(T.score1(R(i):(R(i)+sample_pickup)));
-    R_score2(i) = mean(T.score2(R(i):(R(i)+sample_pickup)));
-    time_start(i) = time(R(i));
-    time_end(i) = time(R(i)+sample_pickup)+shiftT;
+    R_index = R(i):(R(i)+sample_pickup);
+    R_score1(i) = mean(score1(R(i):(R(i)+sample_pickup)));
+    R_score2(i) = mean(score2(R(i):(R(i)+sample_pickup)));
+    time_start(i) = T.time(R(i));
+    time_end(i) = T.time(R(i)+sample_pickup)+shiftT;
 end
-score_total = R_score1+R_score2;
-T_random = table(time_start',time_end',R_score1',R_score2',score_total',...
+R_score_total = R_score1+R_score2;
+T_random = table(time_start',time_end',R_score1',R_score2',R_score_total',...
     'VariableNames',{'t_start','t_end','score1','score2','total'});
 T_random = sortrows(T_random,'total','descend');
 
 %% ピックアップされたオーディオの取得
-for i=1:10
-    readstart = round(T_random.t_start(i)*fs)+1;
-    readend = round(T_random.t_end(i)*fs);
-    a_tmp = audioread([fname_withoutWAV,'.wav'],[readstart,readend]);
-    a_tmp = (a_tmp(:,1)+a_tmp(:,2))/2;
-    audio(i,:) = a_tmp;
-    
-    % write audio
-    s_tmp = int32(T_random.t_start(i));
-    e_tmp = int32(T_random.t_end(i));
-    wfname = ['\Users\Shunji\Music\RandomPickup_141121_001\','rank',num2str(i),...
-        '_time',num2str(s_tmp),'-',num2str(e_tmp),'.wav'];
-    audiowrite(wfname,audio(i,:),fs);
-end
-tmp = readstart-readend;
+% for i=1:10
+%     readstart = round(T_random.t_start(i)*fs)+1;
+%     readend = round(T_random.t_end(i)*fs);
+%     a_tmp = audioread([fname_withoutWAV,'.wav'],[readstart,readend]);
+%     a_tmp = (a_tmp(:,1)+a_tmp(:,2))/2;
+%     audio(i,:) = a_tmp;
+%     
+%     % write audio
+%     s_tmp = int32(T_random.t_start(i));
+%     e_tmp = int32(T_random.t_end(i));
+%     wfname = ['\Users\Shunji\Music\RandomPickup_141121_001\',...
+%        'rank',num2str(i),'_time',num2str(s_tmp),'-',num2str(e_tmp),'.wav'];
+%     audiowrite(wfname,audio(i,:),fs);
+% end
+% 
+% T_sort = sortrows(T,'total','descend');
+% goodaudio = [];
+% fade = 5;
+% m = fade/fs;    % mean
+% sd = m/2;       % standard varidation
+% t = linspace(0,2*m,2*fade);
+% a = -log(0.05)/sd;
+% fadein = 1./(1+exp(-a*(t-m)));
+% fadeout = 1./(1+exp(a*(t-m)));
+% for i=1:1000
+%     readstart = round(T_sort.time(i)*fs)+1;
+%     readend = round((T_sort.time(i)+deltaT)*fs);
+%     a_tmp = audioread([fname_withoutWAV,'.wav'],[readstart,readend]);
+%     a_tmp = (a_tmp(:,1)+a_tmp(:,2))/2;
+%  
+%     a_tmp(1:(fade*2)) = a_tmp(1:(fade*2)).*fadein';
+%     a_tmp((end-fade*2+1):end) = a_tmp((end-fade*2+1):end).*fadeout';
+%     
+%     goodaudio = cat(1,goodaudio,a_tmp);
+% end
+% wfname = ['\Users\Shunji\Music\RandomPickup_141121_001\','goodaudio','.wav'];
+% audiowrite(wfname,goodaudio,fs);
--- soundPickuper_getparameter.m
+++ soundPickuper_getparameter.m
@@ -1,5 +1,5 @@
-function [time,sp,power,cent,chro] = soundPickuper_getparameter...
-    (filename_withoutWAV,s_start,s_end)
+function [time,sp,db,cent,chro] = soundPickuper_getparameter...
+    (filename_withoutWAV,s_start,s_end,deltaT,shiftT,fft_size)
 %% 前処理
 filename_withWAV = [filename_withoutWAV,'.WAV'];
 audio_info = audioinfo(filename_withWAV);
@@ -11,11 +11,12 @@ if audio_info.NumChannels > 1
     audio = (audio(:,1)+audio(:,2))/2;
 end
 
-deltaT = 0.2;   % length of the signal in sec
-shiftT = 0.1;   % shift length in sec
-fft_size = 8192;
+% deltaT = 0.2;   % length of the signal in sec
+% shiftT = 0.1;   % shift length in sec
+% fft_size = 8192;
 fft_min = 10;
 fft_max = 15000;
+P0 = 2e-5;
 %% パラメータ抽出
 ma = miraudio(audio,fs);
 mfr = mirframe(ma,'Length',deltaT,'s','Hop',shiftT/deltaT);
@@ -23,7 +24,8 @@ msp = mirspectrum(mfr,'Length',fft_size,'Min',fft_min,'Max',fft_max);
 mcent = mircentroid(msp,'MaxEntropy',1.0);
 mchro = mirchromagram(mfr);
 fr = mirgetdata(mfr)';
-power = mean(abs(fr),2);
+mean_power = mean(fr.^2,2);
+db = 10*log10(mean_power/P0^2);
 sp = mirgetdata(msp)';
 cent = mirgetdata(mcent)';
 chro = mirgetdata(mchro)';
